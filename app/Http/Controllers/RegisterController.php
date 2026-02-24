f59025403de4aed63f9e4e03a803fe0a05984739
John Ely Ferrer <ferrerjohnely13@gmail.com>
2026-02-24 13:25:42 +0800

    Added email notification upon registration

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers;
 
+use App\Mail\UserRegistrationEmail;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class RegisterController extends Controller
 {
@@ -22,6 +24,8 @@ class RegisterController extends Controller
         ]);
 
         if ($user = User::create($fields)){
+            Mail::to($user->email)->queue(new UserRegistrationEmail($user));
+
             Auth::login($user);
 
             return redirect()->route('home');
---
+++ resources/views/emails/user/register.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Registration</title>
+</head>
+<body>
+    <h1>Greetings, {{ $name }}</h1>
+    
+    <p>You have successfully registered and can now use your account.</p>
+</body>
+</html>
